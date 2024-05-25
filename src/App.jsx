59ea578c8358f13ddd4055b2660cecae1e2d77c3
DioChuks <diochuks65@gmail.com>
2024-05-25 23:58:51 +0100

    feat: infinite scroll commit with sass

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <div className="item item-1"></div>
+      <div className="item item-2"></div>
+      <div className="item item-3"></div>
+      <div className="item item-4"></div>
+      <div className="item item-5"></div>
+      <div className="item item-6"></div>
+      <div className="item item-7"></div>
+      <div className="item item-8"></div>
+    </div>
+  );
+};
+
+export default App;
